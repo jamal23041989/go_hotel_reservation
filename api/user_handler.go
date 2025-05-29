26a6427e7766ca64661b545c104f3145f94ee328
jamal23041989 <jamalkurbanov23041989@gmail.com>
2025-05-29 04:14:35 +0300

    Refactoring types and handler

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,37 @@
 package api
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jamal23041989/go_hotel_reservation/db"
 	"github.com/jamal23041989/go_hotel_reservation/types"
 )
 
-func HandleGetUsers(ctx *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUsers(ctx *fiber.Ctx) error {
 	return ctx.JSON(types.User{
-		ID:        "",
 		FirstName: "Jamal",
 		LastName:  "Kurbanov",
 	})
 }
 
-func HandleGetUser(ctx *fiber.Ctx) error {
-	return ctx.JSON("James")
+func (h *UserHandler) HandleGetUser(ctx *fiber.Ctx) error {
+	var (
+		id = ctx.Params("id")
+		c  = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(c, id)
+	if err != nil {
+		return err
+	}
+	return ctx.JSON(user)
 }
--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/gofiber/fiber/v2"
 	"github.com/jamal23041989/go_hotel_reservation/api"
+	"github.com/jamal23041989/go_hotel_reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
+
+const (
+	dbUri          = "mongodb://localhost:27017/"
+	dbName         = "hotel-reservation"
+	userCollection = "users"
+)
+
+var config = fiber.Config{
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		return ctx.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiV1 := app.Group("/api/v1")
 
-	apiV1.Get("/user", api.HandleGetUsers)
-	apiV1.Get("/user/:id", api.HandleGetUser)
-
+	apiV1.Get("/user", userHandler.HandleGetUsers)
+	apiV1.Get("/user/:id", userHandler.HandleGetUser)
 	app.Listen(*listenAddr)
 }
