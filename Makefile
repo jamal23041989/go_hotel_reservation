301829a405dc0b6b361557e68038c8a876930ace
jamal23041989 <jamalkurbanov23041989@gmail.com>
2025-06-10 03:28:06 +0300

    Create package types and api. Create Makefile

---
+++ Makefile
@@ -0,0 +1,9 @@
+build:
+	@go build -o bin\api
+
+run:
+	@./bin/api
+
+test:
+	@go test -v ./...
+
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/gofiber/fiber/v2"
+	"github.com/jamal23041989/go_hotel_reservation/api"
+)
 
+func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address")
+	flag.Parse()
+
+	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
+	apiv1.Get("/user", api.HandleGetUsers)
+	apiv1.Get("/user/:id", api.HandleGetUser)
+
+	app.Listen(*listenAddr)
 }
